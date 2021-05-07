1d20d02dbb13464f238f95dfd921a8113390e15d
RAHGHOZAR <alirahgozar.dev@gmail.com>
2021-05-07 20:35:29 +0430

    Adding the calculation function

--- app/src/main/java/com/rahgozar/badbakhti_o_meter/MainActivity.java
+++ app/src/main/java/com/rahgozar/badbakhti_o_meter/MainActivity.java
@@ -3,6 +3,15 @@ package com.rahgozar.badbakhti_o_meter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+    public void DayCounter(View view){
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpledateformatYear = new SimpleDateFormat("yyyy");
+        SimpleDateFormat simpledateformatMonth = new SimpleDateFormat("MM");
+        SimpleDateFormat simpledateformatDay = new SimpleDateFormat("dd");
+        int CURRENT_YEAR=Integer.parseInt(simpledateformatYear.format(calendar.getTime()));
+        int CURRENT_MONTH=Integer.parseInt(simpledateformatMonth.format(calendar.getTime()));
+        int CURRENT_DAY=Integer.parseInt(simpledateformatDay.format(calendar.getTime()));
+        int KonKour_DAY=30;
+        int KonKour_YEAR=2022;
+        int KonKour_MONTH=6;
+        String finalDateDayFormat=String.valueOf((KonKour_YEAR-CURRENT_YEAR)*365+(KonKour_MONTH-CURRENT_MONTH)*30+(KonKour_DAY-CURRENT_DAY));
+        TextView SHOWER=(TextView)findViewById(R.id.konkourDayShower);
+        int date=Integer.parseInt(finalDateDayFormat);
+
+        SHOWER.setText(finalDateDayFormat+" DAYS until Konkour \n"+String.valueOf(date/7)+" WEEKS" +" and "+ date%7+" days "+" until Konkour");
 
     }
 }
